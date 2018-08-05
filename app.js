7a247bab55da7b43e960a0d60f0bd687f4de82eb
Juvanto <30640380+Juvanto@users.noreply.github.com>
2018-08-05 17:25:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,17 +16,26 @@ var deleteThing=require('./routes/delete');
 var moodsSubmit=require('./routes/moodsSubmit');
 var homeMoods=require('./routes/homeMoods');
 var moodsEdit=require('./routes/moodsEdit');
-app.use(cookieParser('system'));
+app.use(cookieParser('moodbox'));
+//每次访问，无论访问哪个路径，先把session对象调出来，挂载到req对象上
 app.use(session({
- secret: 'system',
+ secret: 'moodbox',
  resave: true,
- saveUninitialized:true
+ saveUninitialized:true,
+ name:'session_ID'
 }));
 app.use(express.static(path.join(__dirname, '/public')));//设置静态资源目录，放置js、css和图片等文件
 app.use(express.static(path.join(__dirname, 'database/usershead')));//设置多个静态资源目录
 app.use('/', index);//将根目录的访问路由到index.js
 app.use('/signin', signin);
 app.use('/login', login);
+//除非是访问根路径或者是登录注册，否则先验证是否登录，防止未登录的访问
+//req.session.account用以验证是否登录
+app.use(function(req,res,next){if(!req.session.account)
+                                  {res.send('<a href="/">请先登录</a>')}
+                                   else{next();}                    
+                                    }
+                                    )
 app.use('/homepage',homepage); 
 app.use('/infor',infor);
 app.use('/thoughts_submit',thoughtsSubmit);
@@ -38,6 +47,4 @@ app.use('/homepage/moods/moods_edit',moodsEdit);
 app.set('views',path.join(__dirname , 'views'));//设置模版文件目录，类似于静态资源
 app.set('view engine', 'ejs');//设置模版引擎
 app.listen(3000);
-
-//app.engine('html', require('ejs').renderFile);//用ejs模版的方法来渲染html，若用ejs模版则省略这条语句
 //当路径'/'使用了路由，就不具备处理所有请求的能力，如app.use('/',index)，这时并不是所有请求都会经其处理
